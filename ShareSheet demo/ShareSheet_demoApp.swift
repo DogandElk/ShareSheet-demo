3630ea03d610c810e6112359bcefd4adefd0d4c0
Tom Glodek <info@dogandelk.com>
2022-05-05 16:20:26 -0700

    Initial Commit

---
+++ ShareSheet demo/ShareSheet_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheet_demoApp.swift
+//  ShareSheet demo
+//
+//  Created by Thomas GlodekMD on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheet_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
